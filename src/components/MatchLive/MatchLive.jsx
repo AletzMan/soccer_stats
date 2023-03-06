ac7668ca034a534b33656d2bc3f5ba3d01aeffbd
= <=>
2023-03-06 02:31:22 -0600

    Added Tag of minutes of match and status

--- src/components/MatchLive/MatchLive.jsx
+++ src/components/MatchLive/MatchLive.jsx
@@ -7,8 +7,11 @@ function MatchLive({ sportEvent, officials, team, eventActions }) {
     let SCORES_HOME;
     let SCORES_AWAY;
 
-    if (eventActions) {
+   
+    if (eventActions['event-actions-soccer']['action-soccer-score']) {
+        //console.log(eventActions)
         const SCORES = eventActions['event-actions-soccer']['action-soccer-score'];
+        //console.log(SCORES)
         const SCORES_BY_TEAM = SCORES.map((score) => {
             if (score['action-soccer-play-participant'][0]['team-idref'] === HOME_TEAM) {
                 return score = [0, score.minutesElapsed];
@@ -21,15 +24,33 @@ function MatchLive({ sportEvent, officials, team, eventActions }) {
         const PLAYERS_SCORE_AWAY = team[1].player?.map((player) => { return [player.id, player['player-metadata'].name.first, player['player-metadata'].name.last] })
 
         SCORES_HOME = SCORES_BY_TEAM.filter(teamSelect => teamSelect[0] === 0)?.map((teamSelect, index) => {
-            return { minute: teamSelect[1], name: PLAYERS_SCORE_HOME[index][1] + ' ' + PLAYERS_SCORE_HOME[index][2] };
+            return { id: index, minute: teamSelect[1], name: PLAYERS_SCORE_HOME[index][1] + ' ' + PLAYERS_SCORE_HOME[index][2] };
         })
         SCORES_AWAY = SCORES_BY_TEAM.filter(teamSelect => teamSelect[0] === 1)?.map((teamSelect, index) => {
-            return { minute: teamSelect[1], name: PLAYERS_SCORE_AWAY[index][1] + ' ' + PLAYERS_SCORE_AWAY[index][2] };
+            return { id: index,  minute: teamSelect[1], name: PLAYERS_SCORE_AWAY[index][1] + ' ' + PLAYERS_SCORE_AWAY[index][2] };
         })
     }
+    //console.log(sportEvent )
 
-    const STATUS_MATCH = sportEvent['event-status'] === 'post-event' ? 'Finalizado' : 'Previa';
-    const CLASS_STATUS = sportEvent['event-status'] === 'post-event' ? 'finished' : 'uninitiated';
+    let STATUS_MATCH = '';
+    let CLASS_STATUS = '';
+    if(sportEvent['event-status'] === 'post-event') {
+        STATUS_MATCH = 'Finalizado';
+        CLASS_STATUS = 'finished';
+    }
+    if(sportEvent['event-status'] === 'pre-event') {
+        STATUS_MATCH = 'Previa';
+        CLASS_STATUS = 'uninitiated';
+    }
+    if(sportEvent['event-status'] === 'intermission') {
+        STATUS_MATCH = 'Intermedio';
+        CLASS_STATUS = 'inter';
+    }
+    if(sportEvent['event-status'] === 'mid-event') {
+        STATUS_MATCH = 'En Vivo';
+        CLASS_STATUS = 'live';
+    }
+    
     return (
         <>
             <div className="matchlive">
@@ -44,14 +65,15 @@ function MatchLive({ sportEvent, officials, team, eventActions }) {
                 </>
                 <ul className='matchlive__container container__home'>
                     {SCORES_HOME?.map((player) => (
-                        <li className='matchlive__minuteshome matchlive__scores' ><img className='matchlive__img--ball' src={ballImage} alt='image of ball' /> {player.minute + "'" + ' ' + player.name}</li>
+                        <li key={player.id}  className='matchlive__minuteshome matchlive__scores' ><img className='matchlive__img--ball' src={ballImage} alt='image of ball' /> {player.minute + "'" + ' ' + player.name}</li>
                     ))}
                 </ul>
                 <ul className='matchlive__container container__away'>
                     {SCORES_AWAY?.map((player) => (
-                        <li className='matchlive__minutesaway matchlive__scores' ><img className='matchlive__img--ball' src={ballImage} alt='image of ball' /> {player.minute + "'" + ' ' + player.name}</li>
+                        <li key={player.id} className='matchlive__minutesaway matchlive__scores' ><img className='matchlive__img--ball' src={ballImage} alt='image of ball' /> {player.minute + "'" + ' ' + player.name}</li>
                     ))}
                 </ul>
+                <span className={`matchlive__time matchlive__time--${CLASS_STATUS}`}>{`${sportEvent['event-metadata-soccer']['minutes-elapsed']}'`}</span>
                 <span className={`matchlive__status matchlive__status--${CLASS_STATUS}`}>{STATUS_MATCH}</span>
             </div>
         </>
--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -2,9 +2,9 @@ import { MatchLive } from '../MatchLive/MatchLive';
 import './Results.css';
 
 function Results({ results }) {
-    
+    console.log(results)
     const optionsDate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-    const DATE = new Date(results[0]['event-metadata']['start-date-time']).toLocaleDateString('es-MX', optionsDate);
+    const DATE = new Date(results[results.length - 1]['event-metadata']['start-date-time']).toLocaleDateString('es-MX', optionsDate);
     const DATE_FULL = DATE.charAt(0).toUpperCase() + DATE.slice(1);
     return (
         <div className='results'>
